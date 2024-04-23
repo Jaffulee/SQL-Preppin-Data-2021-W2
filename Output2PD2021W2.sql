c39bd7e0e0e30bd2533686f41f17d0d94a7f4b54
Jaffulee <167854664+Jaffulee@users.noreply.github.com>
2024-04-23 12:02:52 +0100

    Create Output2PD2021W2.sql

---
+++ Output2PD2021W2.sql
@@ -0,0 +1,19 @@
+--Use regex to grab text from model
+SELECT REGEXP_SUBSTR(w22021.model,'[A-Z]+') AS brand
+    ,w22021.store
+    ,SUM(w22021.value_per_bike) AS TOTAL_ORDER_VALUE
+    ,SUM(w22021.QUANTITY) AS TOTAL_QUANTITY_SOLD
+    --Convert to date and calculate difference
+    ,ROUND(
+    AVG(
+    DATEDIFF(
+    'day'
+    ,TO_DATE(w22021.order_date,'dd/mm/yyyy')
+    ,TO_DATE(w22021.shipping_date,'dd/mm/yyyy')
+    )
+    )
+    ,1) as AVG_DAYS_TO_SHIP
+
+
+FROM PD2021_WK02_BIKE_SALES AS w22021
+GROUP BY brand, w22021.store
